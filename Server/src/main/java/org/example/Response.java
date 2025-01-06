664f0b47a3f3c402945fb46469e269129d82e5f2
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-06 15:43:10 +0200

    new Response class

---
+++ Server/src/main/java/org/example/Response.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
