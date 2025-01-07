77f0afd785c40ce5d4452afdc97259be17ac34a2
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-07 19:43:53 +0200

    test the db

--- Server/src/main/java/org/example/App.java
+++ Server/src/main/java/org/example/App.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import org.example.database.DatabaseConnection;
+import org.example.database.model.UserEntity;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
 public class App
 {
     public static void main( String[] args )
     {
-        new Server().run();
+//        new Server().run();
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        List<UserEntity> allUsers = databaseConnection.getAllUsers();
+        allUsers.forEach(System.out::println);
     }
 }
