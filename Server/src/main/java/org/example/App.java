8bb5fa411d4b082798fbe63047b5d33b196da0dd
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-09 17:42:13 +0200

    test

--- Server/src/main/java/org/example/App.java
+++ Server/src/main/java/org/example/App.java
@@ -1,23 +1,18 @@
 package org.example;
 
-import org.example.database.dao.RoleEntityDao;
-import org.example.database.model.RoleEntity;
-import org.example.database.model.enums.Role;
+import org.example.database.dao.LocationEntityDao;
 
 public class App
 {
     public static void main( String[] args )
     {
 //        new Server().run();
-        RoleEntityDao roleEntityDao = new RoleEntityDao();
+        try {
+            LocationEntityDao locationEntityDao = new LocationEntityDao();
 
-
-        System.out.println("Roles:");
-        roleEntityDao.getAll().forEach(System.out::println);
-        RoleEntity roleEntity = new RoleEntity();
-        roleEntity.setRole(Role.ADMIN);
-        roleEntityDao.create(roleEntity);
-
-        roleEntityDao.getAll().forEach(System.out::println);
+            locationEntityDao.getAll().forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
