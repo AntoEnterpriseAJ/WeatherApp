1d1302c27675a7dd3292000779d34b1b7a323268
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-10 20:31:51 +0200

    now id auto increments

--- Server/src/main/java/org/example/database/model/UserEntity.java
+++ Server/src/main/java/org/example/database/model/UserEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "\"user\"", schema = "public")
 public class UserEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
