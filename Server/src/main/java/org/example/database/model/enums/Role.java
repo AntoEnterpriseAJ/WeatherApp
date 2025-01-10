1624fe784279dc955c40c009ab99b48fd1a70e8d
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-10 20:28:41 +0200

    Role class util functions

--- Server/src/main/java/org/example/database/model/enums/Role.java
+++ Server/src/main/java/org/example/database/model/enums/Role.java
@@ -1,6 +1,25 @@
 package org.example.database.model.enums;
 
 public enum Role {
-    USER,
-    ADMIN
+    ADMIN(1),
+    USER(2);
+
+    private final int value;
+
+    Role(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Role fromInt(int value) {
+        for (Role role : Role.values()) {
+            if (role.getValue() == value) {
+                return role;
+            }
+        }
+        return null;
+    }
 }
