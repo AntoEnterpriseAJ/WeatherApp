f6693abd83669c307c55abdd468a59346960838d
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-10 20:29:16 +0200

    Client class that holds basic info

---
+++ Server/src/main/java/org/example/server/Client.java
@@ -0,0 +1,42 @@
+package org.example.server;
+
+import org.example.database.model.enums.Role;
+
+public class Client {
+    private String username;
+    private Boolean loggedIn = false;
+    private Role role = Role.USER;
+    private long locationId;
+
+    public Boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(long locationId) {
+        this.locationId = locationId;
+    }
+}
